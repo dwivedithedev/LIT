4704861f7e404abe36817d1d8cf6e4587dc5a320
Vaibhav Dwivedi <vaibhavforeverpr@gmail.com>
2020-10-03 19:52:16 +0530

    img issues and suggestion page fixed

--- src/Suggestions.svelte
+++ src/Suggestions.svelte
@@ -71,9 +71,7 @@
     </div>
 
     <!-- Show the embedded form here-->
-      <div class="textBox text-center">
-        <div class="typeform-widget" data-url="https://form.typeform.com/to/kMMVYmum" data-transparency="50" data-hide-headers="true" data-hide-footer="true" style="width: 100%; height: 300px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> 
-      </div>
+	<a class="typeform-share button" href="https://form.typeform.com/to/kMMVYmum" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#3A7685;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:25px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Launch me </a> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
   
   <div class="p-50">
     <Footer/>
